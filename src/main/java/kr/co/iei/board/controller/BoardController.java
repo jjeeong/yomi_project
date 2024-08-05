b49aecbb7b40a84819c16ae9b0c8496d7a4a71a8
임형묵 <limhm1221@naver.com>
2024-08-05 15:45:06 +0900

    리스트, 작성Frm 추가

--- src/main/java/kr/co/iei/board/controller/BoardController.java
+++ src/main/java/kr/co/iei/board/controller/BoardController.java
@@ -1,18 +1,35 @@
 package kr.co.iei.board.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.co.iei.board.model.service.BoardService;
 
 @Controller
-
+@RequestMapping(value="/board")
 public class BoardController {
 	@Autowired
 	private BoardService boardService;
 	
 	@Value("${file.root}")
 	private String root;
-
+	
+	@GetMapping(value="/list")
+	public String list(Model model) {
+		List list = boardService.selectBoardList();
+		model.addAttribute("list",list);
+		
+		return "/board/list";
+	}
+	
+	@GetMapping(value="/writeFrm")
+	public String writeFrm() {
+		return "board/writeFrm";
+	}
 }
--- src/main/java/kr/co/iei/board/model/service/BoardService.java
+++ src/main/java/kr/co/iei/board/model/service/BoardService.java
@@ -1,5 +1,7 @@
 package kr.co.iei.board.model.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,4 +12,12 @@ public class BoardService {
 	
 	@Autowired
 	private BoardDao boardDao;
-}
+
+	public List selectBoardList() {
+		List list =  boardDao.selectBoardList();
+		return list;
+	}
+
+
+	}
+
