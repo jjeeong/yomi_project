a187e5ecc0e38be394f4338c021aeec94168032a
임형묵 <limhm1221@naver.com>
2024-08-05 17:39:15 +0900

    summerNote 완성 !

--- src/main/java/kr/co/iei/board/controller/BoardController.java
+++ src/main/java/kr/co/iei/board/controller/BoardController.java
@@ -2,6 +2,7 @@ package kr.co.iei.board.controller;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -9,14 +10,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import kr.co.iei.board.model.service.BoardService;
+import kr.co.iei.util.FileUtils;
 
 @Controller
 @RequestMapping(value="/board")
 public class BoardController {
 	@Autowired
 	private BoardService boardService;
+	
+	@Autowired
+	private FileUtils fileUtils;
 	
 	@Value("${file.root}")
 	private String root;
@@ -33,5 +40,12 @@ public class BoardController {
 	public String writeFrm() {
 		return "board/writeFrm";
 	}
-
+	
+	@ResponseBody
+	@PostMapping(value="/editorImage", produces ="plain/text;charset=utf-8")
+	public String editorImage(MultipartFile upfile) {
+		String savepath = root+"/board/editor/";
+		String filepath = fileUtils.upload(savepath, upfile);
+		return "/board/editor/"+filepath;
+	}
 }
